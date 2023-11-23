2e7527aecbcda13849c17106096e0b7692d199b6
mianazeemdaula <mianazeemdaula@gmail.com>
2023-11-23 21:04:33 +0500

    as

--- app/Http/Controllers/Api/DepositController.php
+++ app/Http/Controllers/Api/DepositController.php
@@ -34,7 +34,7 @@ class DepositController extends Controller
                 "is_fee_paid_by_user" => false
             ];
             $payment = (new NowPayment())->payment($data);
-            // return response()->json($payment);
+            return response()->json($payment);
             return response()->json([
                 'address' => $payment['pay_address'],
                 'amount' => $payment['price_amount'],
