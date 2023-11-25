14207f3148f49d9e903ff1f0aa22d7b8dd316c65
mianazeemdaula <mianazeemdaula@gmail.com>
2023-11-25 12:01:18 +0500

    reqest

--- app/Http/Controllers/Api/PayoutController.php
+++ app/Http/Controllers/Api/PayoutController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Withdrawl;
 
 class PayoutController extends Controller
 {
@@ -13,16 +14,22 @@ class PayoutController extends Controller
             'address' => 'required|string',
         ]);
         $user = auth()->user();
-        if(($user->wallet->balance ?? 0) < $data['amount']){
-            return response()->json([
-                'message' => 'Insufficient balance'
-            ], 422);
-        }
-        if($user->paidReferrals()->count() < 7){
-            return response()->json([
-                'message' => 'You need at least 7 referrals to request payout'
-            ], 422);
-        }
+        // if(($user->wallet->balance ?? 0) < $data['amount']){
+        //     return response()->json([
+        //         'message' => 'Insufficient balance'
+        //     ], 422);
+        // }
+        // if($user->paidReferrals()->count() < 7){
+        //     return response()->json([
+        //         'message' => 'You need at least 7 referrals to request payout'
+        //     ], 422);
+        // }
+        $payout = new Withdrawl();
+        $payout->user_id = $user->id;
+        $payout->amount = $data['amount'];
+        $payout->account = $data['address'];
+        $payout->payment_method_id = 1;
+        $payout->save();
         return response()->json([
             'message' => 'Payout request sent successfully'
         ]);
