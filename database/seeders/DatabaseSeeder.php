0f9898f39bdada8de46cb8eabeaf354b9383ad69
mianazeemdaula <mianazeemdaula@gmail.com>
2023-11-26 13:11:32 +0500

    as

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -106,8 +106,6 @@ class DatabaseSeeder extends Seeder
         ]);
 
         \App\Models\User::factory(10)->create();
-        \App\Models\Package::factory(5)->create();
-
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
