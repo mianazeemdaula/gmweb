eb0c1d257f3fa1810b934ff58fc49684ee383ca9
mianazeemdaula <mianazeemdaula@gmail.com>
2023-11-30 13:30:43 +0500

    triat

--- app/Traits/TransactionTrait.php
+++ app/Traits/TransactionTrait.php
@@ -9,29 +9,35 @@ trait TransactionTrait
 {
     public function updateWallet(float $amount, string $description = null, bool $isBonus = false)
     {
-        $wallet =  $this->wallet;
-        $balance = 0;
-        if($wallet){
-            $balance = $wallet->balance;
+        try {
+            DB::beginTransaction();
+            $wallet =  $this->transactions()->latest()->first();
+            $balance = 0;
+            if($wallet){
+                $balance = $wallet->balance;
+            }
+            $debit = $credit = 0;
+            if($amount > 0){
+                $balance = $balance + $amount;
+                $credit = $amount;
+            }else{
+                $balance = $balance - abs($amount);
+                $debit = abs($amount);
+            }
+            $transaction = new Wallet([
+                'debit' => $debit,
+                'credit' => $credit,
+                'balance' =>  $balance,
+                'description' => $description,
+                'is_bonus' => $isBonus,
+                'user_id' => $this->id,
+            ]);
+            // Associate the transaction with the user
+            $this->transactions()->save($transaction);
+            DB::commit();
+        } catch (\Throwable $th) {
+            //throw $th;
+            DB::rollBack();
         }
-        $debit = $credit = 0;
-        if($amount > 0){
-            $balance = $balance + $amount;
-            $credit = $amount;
-        }else{
-            $balance = $balance - abs($amount);
-            $debit = abs($amount);
-        }
-        $transaction = new Wallet([
-            'debit' => $debit,
-            'credit' => $credit,
-            'balance' =>  $balance,
-            'description' => $description,
-            'is_bonus' => $isBonus,
-            'user_id' => $this->id,
-        ]);
-
-        // Associate the transaction with the user
-        $this->transactions()->save($transaction);
     }
 }
