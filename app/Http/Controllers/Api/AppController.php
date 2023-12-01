64940d92cee32091b9531a18b64acb31606a8715
mianazeemdaula <mianazeemdaula@gmail.com>
2023-12-01 10:41:27 +0500

    as

--- app/Http/Controllers/Api/AppController.php
+++ app/Http/Controllers/Api/AppController.php
@@ -42,7 +42,7 @@ class AppController extends Controller
         }else{
             $this->fetch();
         }
-        $data['app_version'] = '1.0.0';
+        $data['app_version'] = '1.0.1';
         $data['app_name'] = 'Crypto MLM';
         $data['app_logo'] = asset('images/logo.png');
         $data['currencies'] = Currency::whereIn('name',['PKR','USD','AED','ZAR','SAR'])->get();
